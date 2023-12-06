a15ff6f8e2fee288117e1b53169560d43eff865a
AnhNT <mtgtest2.vn@gmail.com>
2023-12-06 15:44:17 +0700

    Update click record

---
+++ app/src/main/java/com/mtg/app/voicechanger/utils/FirebaseUtils.kt
@@ -0,0 +1,37 @@
+package com.mtg.app.voicechanger.utils
+
+import android.content.Context
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+object FirebaseUtils {
+    @JvmStatic
+    fun sendEventSubmitRate(context: Context?, comment: String?, rate: Int) {
+        val bundle = Bundle()
+        bundle.putString("event_type", "rated")
+        bundle.putString("comment", comment)
+        bundle.putString("star", "$rate star")
+        FirebaseAnalytics.getInstance(context!!).logEvent("prox_rating_layout", bundle)
+    }
+
+    @JvmStatic
+    fun sendEventLaterRate(context: Context?) {
+        val bundle = Bundle()
+        bundle.putString("event_type", "cancel")
+        FirebaseAnalytics.getInstance(context!!).logEvent("prox_rating_layout", bundle)
+    }
+
+    @JvmStatic
+    fun sendEventChangeRate(context: Context?, rate: Int) {
+        val bundle = Bundle()
+        bundle.putString("event_type", "rated")
+        bundle.putString("star", "$rate star")
+        FirebaseAnalytics.getInstance(context!!).logEvent("prox_rating_layout", bundle)
+    }
+
+    fun sendEvent(context: Context?, nameEvent: String?, typeEvent: String?) {
+        val bundle = Bundle()
+        bundle.putString("event_type", typeEvent)
+        FirebaseAnalytics.getInstance(context!!).logEvent(nameEvent!!, bundle)
+    }
+}
